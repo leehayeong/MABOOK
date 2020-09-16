2552b4ac384d983356fdab379055cce24c287a98
leehayeong <lhaayyy@naver.com>
2020-09-16 10:04:54 +0900

    [3] RecyclerView 로 검색 결과 10건 나타내기 (이미지 glide 사용)

--- app/src/main/java/com/example/mybook/MainActivity.kt
+++ app/src/main/java/com/example/mybook/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.mybook
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView.VERTICAL
+import com.example.mybook.adapter.BookAdapter
 import com.example.mybook.model.Book
 import com.example.mybook.retrofit.NaverApi
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,11 +19,17 @@ class MainActivity : AppCompatActivity() {
     private val query = "책"
     private val display = 10
 
+    private val bookAdapter: BookAdapter by lazy {
+        BookAdapter(mutableListOf())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         searchBook()
+        rv_book_list.adapter = bookAdapter
+        rv_book_list.addItemDecoration(DividerItemDecoration(applicationContext, VERTICAL))
     }
 
     private fun searchBook() {
@@ -30,6 +39,7 @@ class MainActivity : AppCompatActivity() {
                 val total = response.body()?.total ?: 0
                 val itemList = response.body()?.items ?: emptyList()
 
+                bookAdapter.addItem(itemList)
                 setText(total)
 
                 Log.i("호출 성공", "${response.body()}")
@@ -41,7 +51,7 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun setText(total: Int){
+    private fun setText(total: Int) {
         tv_total.text = "$query 검색 결과 총 $total 건"
     }
 }
---
+++ app/src/main/java/com/example/mybook/adapter/BookAdapter.kt
@@ -0,0 +1,56 @@
+package com.example.mybook.adapter
+
+import android.os.Build
+import android.text.Html
+import android.text.Spanned
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.mybook.R
+import com.example.mybook.model.Item
+import kotlinx.android.synthetic.main.item_book.view.*
+
+class BookAdapter(private val itemList: MutableList<Item>) :
+    RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
+        return BookViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_book, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
+        holder.apply {
+            bind(itemList[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    fun addItem(itemList: List<Item>) {
+        this.itemList.addAll(itemList)
+        notifyDataSetChanged()
+    }
+
+    class BookViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(item: Item) {
+            Glide.with(view).load(item.image).into(view.iv_book_image)
+            view.tv_book_title.text = htmlToString(item.title)
+            view.tv_book_author.text = htmlToString(item.author)
+            view.tv_book_publisher.text = htmlToString(item.publisher)
+            view.tv_book_price.text = item.price
+            view.tv_book_discount.text = item.discount
+        }
+
+        private fun htmlToString(string: String): Spanned {
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                Html.fromHtml(string, Html.FROM_HTML_MODE_LEGACY)
+            } else {
+                @Suppress("DEPRECATION")
+                Html.fromHtml(string)
+            }
+        }
+    }
+}
